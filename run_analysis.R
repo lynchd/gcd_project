663bee53f2785cf0fd15f9bf1e5e0cf0b7924a41
David Lynch <lynchdt@gmail.com>
2015-08-21 11:35:58 +0100

    Final clean up

--- run_analysis.R
+++ run_analysis.R
@@ -14,33 +14,25 @@ unzipDataSetWithOverwrite <- function(file) {
 }
 
 # File names
-zipFile <-"dataset.zip"                                              # Full archive as downloaded
-featuresFile <- "UCI HAR Dataset/features.txt"                       # Variable names for samples in all sets
-trainSetFile <- "UCI HAR Dataset/train/X_train.txt"                  # Training set file
-trainLabelsFile <- "UCI HAR Dataset/train/y_train.txt"               # Activity labels that apply to the training set file
-trainSubjectFile <- "UCI HAR Dataset/train/subject_train.txt"        # Subject records for training
-testSetFile <- "UCI HAR Dataset/test/X_test.txt"                     # Test set file
-testLabelsFile <- "UCI HAR Dataset/test/Y_test.txt"                  # Activity labels that apply to the test set file
-testSubjectFile <- "UCI HAR Dataset/test/subject_test.txt"           # Subject records for test
-activityLabelFile <- "UCI HAR Dataset/activity_labels.txt"           # Labels for activity levels
-newLabelFile <- "new_headers.txt"                                    # Improved column headers
+                             # Improved column headers
 
 # Download and unzip the data
 # This will leave a UCI HAR dataset folder in the working directory
 URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-fetchDataSetIfRequired(URL, zipFile);
-unzipDataSetWithOverwrite(zipFile);
+fetchDataSetIfRequired(URL, "dataset.zip" );
+unzipDataSetWithOverwrite("dataset.zip" );
+
+dataRoot <- "UCI HAR Dataset"
 
 # Load up the data
-features <- read.table(featuresFile)
-trainSet <- read.table(trainSetFile)
-trainLabel <- read.table(trainLabelsFile)
-trainSubject <- read.table(trainSubjectFile)
-testSet <- read.table(testSetFile)
-testLabel <- read.table(testLabelsFile)
-testSubject <- read.table(testSubjectFile)
-activityLabel <- read.table(activityLabelFile)
-newHeaders <- read.table(newLabelFile)
+features <- read.table(file.path(dataRoot, "features.txt"))
+trainSet <- read.table(file.path(dataRoot, "train", "X_train.txt"))
+trainLabel <- read.table(file.path(dataRoot, "train", "y_train.txt"))
+trainSubject <- read.table(file.path(dataRoot, "train", "subject_train.txt"))
+testSet <- read.table(file.path(dataRoot, "test", "X_test.txt"))
+testLabel <- read.table(file.path(dataRoot, "test", "y_test.txt"))
+testSubject <- read.table(file.path(dataRoot, "test", "subject_test.txt"))
+activityLabel <- read.table(file.path(dataRoot, "activity_labels.txt"))
 
 # Apply meaningful column names to both data-sets
 colnames(trainSet) <- features[,2]
@@ -79,7 +71,8 @@ mergedDf$activity_label <- factor(mergedDf$activity_label)
 # Apply the labels to the levels
 levels(mergedDf$activity_label) <- activityLabel[,2]
 
-# Set improved column headers - see code book for details
+# Set improved column headers - specified in different file see code book for details
+newHeaders <- read.table("new_headers.txt")
 colnames(mergedDf) <- newHeaders[,1] 
 
 # Create new average data set that groups the data by subject ID and activity label. 
